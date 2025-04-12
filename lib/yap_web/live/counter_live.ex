bd3a1652bcae1c62c95f9ccfcb1cfc89fdd10221
Nelson Estevão <nelsonmestevao@proton.me>
2025-04-12 09:04:40 +0100

    solution: counter

--- lib/yap_web/live/counter_live.ex
+++ lib/yap_web/live/counter_live.ex
@@ -8,10 +8,10 @@ defmodule YapWeb.CounterLive do
       <div class="flex flex-col items-center justify-center">
         <div class="p-6 w-80 text-center">
           <h1 class="text-3xl font-bold mb-4">Counter</h1>
-          <p class="text-4xl font-semibold text-primary mb-6">Zero</p>
+          <p class="text-4xl font-semibold text-primary mb-6">{@count}</p>
           <div class="flex justify-center space-x-6">
-            <.button>+</.button>
-            <.button>-</.button>
+            <.button phx-click="increment">+</.button>
+            <.button phx-click="decrement">-</.button>
           </div>
         </div>
       </div>
@@ -21,6 +21,16 @@ defmodule YapWeb.CounterLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, count: 0)}
+  end
+
+  @impl true
+  def handle_event("increment", _params, socket) do
+    {:noreply, assign(socket, count: socket.assigns.count + 1)}
+  end
+
+  @impl true
+  def handle_event("decrement", _params, socket) do
+    {:noreply, assign(socket, count: socket.assigns.count - 1)}
   end
 end
