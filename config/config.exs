29314c9a3b04fc8327e225029dc2bb65df1a902a
Nelson Estevão <nelsonmestevao@proton.me>
2025-04-12 10:41:00 +0100

    solution: create authentication

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :yap, :scopes,
+  user: [
+    default: true,
+    module: Yap.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: Yap.AccountsFixtures,
+    test_login_helper: :register_and_log_in_user
+  ]
+
 config :yap,
   ecto_repos: [Yap.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
--- lib/yap_web/router.ex
+++ lib/yap_web/router.ex
@@ -1,6 +1,8 @@
 defmodule YapWeb.Router do
   use YapWeb, :router
 
+  import YapWeb.UserAuth
+
   import YapWeb.UserTracking
 
   pipeline :browser do
@@ -10,6 +12,7 @@ defmodule YapWeb.Router do
     plug :put_root_layout, html: {YapWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
     plug :ensure_anonymous_cookie
   end
 
@@ -55,5 +58,33 @@ defmodule YapWeb.Router do
       live_dashboard "/dashboard", metrics: YapWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", YapWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{YapWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", YapWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{YapWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
