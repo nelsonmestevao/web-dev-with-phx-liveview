4413b564be4d8bf11dbc407199cbe5dc0a2d200b
Nelson Estevão <nelsonmestevao@proton.me>
2025-04-12 09:12:17 +0100

    feat: add initial global counter

--- lib/yap_web/live/global_counter_live.ex
+++ lib/yap_web/live/global_counter_live.ex
@@ -6,12 +6,34 @@ defmodule YapWeb.GlobalCounterLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <span>Global Counter: {@count}</span>
+    <Layouts.app flash={@flash}>
+      <div class="flex flex-col items-center justify-center">
+        <div class="p-6 w-80 text-center">
+          <h1 class="text-3xl font-bold mb-4">Global Counter</h1>
+          <p class="text-4xl font-semibold text-primary mb-6">{@count}</p>
+          <div class="flex justify-center space-x-6">
+            <.button phx-click="increment">+</.button>
+            <.button phx-click="decrement">-</.button>
+          </div>
+        </div>
+      </div>
+    </Layouts.app>
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, count: GlobalCounter.get())}
+  end
+
+  def handle_event("increment", _params, socket) do
+    GlobalCounter.increment()
+    {:noreply, assign(socket, count: GlobalCounter.get())}
+  end
+
+  @impl true
+  def handle_event("decrement", _params, socket) do
+    GlobalCounter.decrement()
+    {:noreply, assign(socket, count: GlobalCounter.get())}
   end
 end
